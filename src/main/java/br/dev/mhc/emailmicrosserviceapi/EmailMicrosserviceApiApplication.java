8da5d6ce79abc3966a0b19e595be52715062bd49
Marcelo Cezário <marcelocezario@gmail.com>
2021-07-20 21:15:07 -0300

    Criando Projeto

---
+++ src/main/java/br/dev/mhc/emailmicrosserviceapi/EmailMicrosserviceApiApplication.java
@@ -0,0 +1,13 @@
+package br.dev.mhc.emailmicrosserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailMicrosserviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailMicrosserviceApiApplication.class, args);
+	}
+
+}
